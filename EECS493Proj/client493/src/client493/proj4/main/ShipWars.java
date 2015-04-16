5e4b284b65c968db0ca6502c6d2e9bdc876bd061
Troy <nemetro@umich.edu>
2015-04-16 17:58:32 -0400

    cleaned up

--- EECS493Proj/client493/src/client493/proj4/main/ShipWars.java
+++ EECS493Proj/client493/src/client493/proj4/main/ShipWars.java
@@ -12,14 +12,15 @@ import javax.swing.WindowConstants;
 
 public class ShipWars{
   public static void main(String [] args){
-    /*WelcomeGUI welcome = new WelcomeGUI("Ship Wars");
+    WelcomeGUI welcome = new WelcomeGUI("Ship Wars");
     welcome.pack();
     welcome.setVisible(true);
     welcome.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-    */
+    
 	
+	/*TROY'S Testing
 	Player player = new Player("Boo", "0.0.0.0");
-      
+    
     final SetupShips setup = new SetupShips("ShipWars Matchmaking", player);
     setup.pack();
     setup.setVisible(true);
@@ -27,6 +28,7 @@ public class ShipWars{
     setup.setMaximumSize(new Dimension(668, 396));
     setup.setPreferredSize(new Dimension(668, 396));
     setup.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    */
 	  
     /*WinningDialog win = new WinningDialog(false, "Dude");
     win.pack();
